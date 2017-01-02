473f34589d0c59ed240f3908583c9710eff0adf3
Austin Blain and Joshua Rinard <austin+joshua@>
2017-01-02 10:45:16 -0800

    minify and clean and build

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
 
+var buildProduction = utilities.env.production; // talks to utilities
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -15,4 +19,22 @@ gulp.task('jsBrowserify', ['concatInterface'], function(){
   .bundle()
   .pipe(source('app.js'))
   .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("build", function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
 });
